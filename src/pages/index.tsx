fd7a6cf7bd6803b4cec022eb770666dd0b5675f1
Agatha Ambrose <agathambrose@gmail.com>
2022-02-13 13:36:51 +0000

    Second base setup with improvements

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+		<div>
+			<Head>
+				<title>Bluechip - Home</title>
+			</Head>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	mode: "jit",
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				"Noto Serif": ["Noto Sans", "ui-sans-serif", "ui-serif"],
+			},
+			colors: {
+				"primary-color": "#1B75BC",
+				"secondary-color": "#0F1A42",
+				"box-color": "#A8A8A8",
+			},
+		},
+	},
+	plugins: [],
+};
