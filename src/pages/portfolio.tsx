d7b18db9163954b58e5f5b270e2659ef0b116203
Agatha Ambrose <agathambrose@gmail.com>
2022-02-27 16:31:20 +0000

    Portfolio page improved

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -137,29 +137,32 @@ const Portfolio = () => {
 						</div>
 					</div>
 					<div className="px-4 md:block hidden">
-						<div className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden">
-							<div className="flex flex-col mr-8 space-y-2 justify-start lg:w-2/6">
-								<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
-									East End
-								</h3>
-								<p className="text-gray-600 text-lg">
-									East End Hospitality Limited, incorporated in September 2020,
-									is a bistro restaurant in Ghana and the second establishment
-									of Blue Chip. East End bistro pairs cuisine and a unique
-									ambiance that does not only represent the modern Ghanaian but
-									also a highlights the Ghanaian culture that will make you
-									embrace all your traditions as you dine. East Ends’ vision is
-									to be one of the best restaurants in the world hence striving
-									for the world class attitude. East End seeks to provide its
-									customers with an affordable casual and comfortable experience
-									at a restaurant whiles making memories. East End has a diverse
-									client base, ranging between the ages of 16 to 55 years old.
-									The East End space has a capacity of 180 customers. East End
-									receives an average of 560 customers per week with each
-									customer spending an average of GH₵ 125.
-								</p>
+						<div className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden relative">
+							<div className="w-full bg-blue-400">
+								<div className="flex absolute md:-top-56 lg:left-[12rem] lg:-top-16 md:left-10 flex-col mr-8 space-y-2 justify-start lg:w-1/3 md:w-5/12">
+									<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
+										East End
+									</h3>
+									<p className="text-gray-600 text-lg">
+										East End Hospitality Limited, incorporated in September
+										2020, is a bistro restaurant in Ghana and the second
+										establishment of Blue Chip. East End bistro pairs cuisine
+										and a unique ambiance that does not only represent the
+										modern Ghanaian but also a highlights the Ghanaian culture
+										that will make you embrace all your traditions as you dine.
+										East Ends’ vision is to be one of the best restaurants in
+										the world hence striving for the world class attitude. East
+										End seeks to provide its customers with an affordable casual
+										and comfortable experience at a restaurant whiles making
+										memories. East End has a diverse client base, ranging
+										between the ages of 16 to 55 years old. The East End space
+										has a capacity of 180 customers. East End receives an
+										average of 560 customers per week with each customer
+										spending an average of GH₵ 125.
+									</p>
+								</div>
 							</div>
-							<div className="relative flex lg:justify-center">
+							<div className="relative w-full flex lg:justify-start ml-12">
 								<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
 									<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
 										<Image
@@ -169,7 +172,7 @@ const Portfolio = () => {
 											height="380px"
 											className="rounded-2xl absolute z-10"
 										/>
-										<div className="h-[100px] w-[150px] rounded-tl-3xl z-[1] absolute -top-2 md:-left-1 lg:-left-2 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
+										<div className="h-[100px] w-[150px] rounded-tl-3xl z-[1] absolute -top-2 md:-left-2 lg:-left-2 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
 									</div>
 								</div>
 							</div>
@@ -292,24 +295,27 @@ const Portfolio = () => {
 					</div>
 					<div className="px-4 md:block hidden my-3">
 						<div className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden">
-							<div className="flex flex-col mr-8 space-y-2 justify-start lg:w-2/6">
-								<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
-									Scorch
-								</h3>
-								<p className="text-gray-600 text-lg">
-									Scorch Restaurant Limited, incorporated in March 2020, is a
-									rodizio concept restaurant and will be Blue Chip’s forth
-									establishment. Scorch is a rodizio concept restaurant with a
-									local twist to meet the needs of Ghanaians. Scotch will serve
-									a blend of fine dining meals in a relaxed and elegant
-									environment. It seeks to bring its customers an unforgettable
-									premium experience. Scorch will cater to the middle- and
-									upper-class section of the Ghanaian society between the ages
-									of 16 to 55 years old with our services. The restaurant
-									occupies one floor and can seat up to 450 customers.
-								</p>
+							<div className="w-full relative">
+								<div className="flex flex-col mr-8 space-y-2 justify-start lg:w-4/6 md:w-2/6 lg:-top-56 lg:left-40 absolute -top-[22rem]">
+									<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
+										Scorch
+									</h3>
+									<p className="text-gray-600 text-lg">
+										Scorch Restaurant Limited, incorporated in March 2020, is a
+										rodizio concept restaurant and will be Blue Chip’s forth
+										establishment. Scorch is a rodizio concept restaurant with a
+										local twist to meet the needs of Ghanaians. Scotch will
+										serve a blend of fine dining meals in a relaxed and elegant
+										environment. It seeks to bring its customers an
+										unforgettable premium experience. Scorch will cater to the
+										middle- and upper-class section of the Ghanaian society
+										between the ages of 16 to 55 years old with our services.
+										The restaurant occupies one floor and can seat up to 450
+										customers.
+									</p>
+								</div>
 							</div>
-							<div className="relative flex lg:justify-center">
+							<div className="relative w-full flex lg:justify-start ml-12">
 								<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
 									<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
 										<Image
@@ -319,7 +325,7 @@ const Portfolio = () => {
 											height="380px"
 											className="rounded-2xl absolute z-10"
 										/>
-										<div className="h-[100px] w-[150px] rounded-tl-3xl z-[1] absolute -top-2 md:-left-1 lg:-left-2 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
+										<div className="h-[100px] w-[150px] rounded-tl-3xl z-[1] absolute -top-2 md:-left-2 lg:-left-2 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
 									</div>
 								</div>
 							</div>
