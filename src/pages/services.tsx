6609ee9c7a9ca4ee9af4b54bf9cb2eadaefa1289
Agatha Ambrose <agathambrose@gmail.com>
2022-03-03 15:58:49 +0000

    More adjustments, phew!

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -25,7 +25,8 @@ const Services = () => {
 					</div>
 				</div>
 				<div className="px-4 pt-4">
-					<div className="md:hidden" data-aos="fade-in">
+					<div className="md:hidden" data-aos="fade-in"
+					>
 						<div className="relative my-8 px-5 md:px-0 lg:my-0 w-full flex justify-center h-[330px]">
 							<div className="md:hidden h-[1793px] w-[1px] bg-gray-400 top-24 absolute left-4"></div>
 							<div className="h-[300px] w-[500px] md:block hidden z-10 top-3 absolute">
@@ -72,7 +73,7 @@ const Services = () => {
 						></div>
 						<div
 							className="relative flex lg:justify-center mr-8"
-							data-aos="fade-left"
+							data-aos="fade-up"
 						>
 							<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
 								<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
@@ -83,7 +84,7 @@ const Services = () => {
 										height="380px"
 										className="rounded-2xl absolute z-10"
 									/>
-									<div className="h-[100px] w-[150px] rounded-tl-2xl z-[1] absolute -top-2 -left-3 md:left-0 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
+									<div className="h-[100px] w-[150px] rounded-tl-2xl z-[1] absolute -top-2 lg:-left-2 -left-3 md:left-0 opacity-30 bg-gradient-to-tr from-primary-color via-primary-color to-white shadow-2xl shadow-white"></div>
 								</div>
 							</div>
 						</div>
@@ -108,7 +109,9 @@ const Services = () => {
 					</div>
 				</div>
 				<div className="px-4 md:hidden block">
-					<div className="md:hidden" data-aos="fade-in">
+					<div className="md:hidden"
+						data-aos="fade-in"
+					>
 						<div className="relative my-8 px-5 md:px-0 lg:my-0 w-full flex justify-center h-[290px]">
 							<div className="h-[600px] block md:hidden top-3 absolute">
 								<Image
@@ -123,7 +126,7 @@ const Services = () => {
 						</div>
 						<div
 							className="flex flex-col items-center space-y-2 justify-center"
-							data-aos="fade-up"
+							data-aos="fade-down"
 						>
 							<h3 className="text-3xl md:text-4xl font-black text-secondary-color mt-3">
 								Construction
@@ -142,7 +145,9 @@ const Services = () => {
 						</div>
 					</div>
 				</div>
-				<div className="px-4 md:block hidden" data-aos="fade-in">
+				<div className="px-4 md:block hidden"
+					data-aos="fade-in"
+				>
 					<div className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden relative">
 						<div className="h-[1815px] w-[1px] bg-gray-400 bottom-5 md:top-20 lg:-top-6 absolute right-0 md:left-5 lg:left-14"></div>
 						<div className="flex flex-col ml-6 mr-8 space-y-2 justify-start lg:w-3/6">
@@ -163,7 +168,7 @@ const Services = () => {
 						</div>
 						<div
 							className="relative flex lg:justify-center"
-							data-aos="fade-up-right"
+							data-aos="fade-up"
 						>
 							<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
 								<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
@@ -181,7 +186,9 @@ const Services = () => {
 					</div>
 				</div>
 				<div className="px-4 py-2 md:py-4 my-8 bg-blue-700">
-					<div className="md:hidden mobile" data-aos="fade-in">
+					<div className="md:hidden mobile"
+						data-aos="fade-in"
+					>
 						<div className="relative px-5 md:px-0 lg:my-0 w-full flex justify-center h-[280px]">
 							<div className="h-[400px] w-[500px] md:block hidden z-10 top-3 absolute">
 								<Image
@@ -222,7 +229,7 @@ const Services = () => {
 					</div>
 					<div
 						className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden tablet-desktop"
-						data-aos="fade-down-left"
+						data-aos="fade-down"
 					>
 						<div className="relative flex lg:justify-center mr-8">
 							<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
@@ -258,7 +265,9 @@ const Services = () => {
 						</div>
 					</div>
 				</div>
-				<div className="px-4 md:hidden block" data-aos="fade-down">
+				<div className="px-4 md:hidden block"
+					data-aos="fade-down"
+				>
 					<div className="md:hidden relative">
 						<div className="md:hidden h-[1680px] w-[1px] bg-gray-400 top-24 absolute left-4"></div>
 						<div className="relative my-8 px-5 md:px-0 lg:my-0 w-full flex justify-center h-[290px]">
@@ -296,7 +305,7 @@ const Services = () => {
 				<div className="px-4 md:block hidden" data-aos="fade-in">
 					<div
 						className="md:flex w-full justify-center items-center lg:px-10 md:px-3 pt-4 hidden"
-						data-aos="fade-left"
+						data-aos="fade-up"
 					>
 						<div className="flex flex-col ml-8 mr-8 space-y-2 justify-start lg:w-4/6">
 							<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
@@ -315,7 +324,7 @@ const Services = () => {
 						</div>
 						<div
 							className="relative flex lg:justify-center"
-							data-aos="fade-right"
+							data-aos="fade-in"
 						>
 							<div className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]">
 								<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
@@ -333,7 +342,9 @@ const Services = () => {
 					</div>
 				</div>
 				<div className="px-4 py-4">
-					<div className="md:hidden" data-aos="fade-in">
+					<div className="md:hidden"
+						data-aos="fade-in"
+					>
 						<div className="relative my-8 px-5 md:px-0 lg:my-0 w-full flex justify-center h-[280px]">
 							<div className="h-[600px] block md:hidden z-10 top-3 absolute">
 								<Image
@@ -374,7 +385,7 @@ const Services = () => {
 						<div className="relative flex lg:justify-center mr-8">
 							<div
 								className="relative my-8 lg:my-0 lg:w-[400px] md:w-[350px] flex justify-center lg:h-[400px] md:h-[350px]"
-								data-aos="fade-down-right"
+								data-aos="fade-up"
 							>
 								<div className="lg:h-[400px] lg:w-[400px] md:w-[350px] md:h[380px] z-10 top-3 absolute">
 									<Image
@@ -390,7 +401,7 @@ const Services = () => {
 						</div>
 						<div
 							className="flex flex-col space-y-2 justify-start lg:w-5/6"
-							data-aos="fade-up-left"
+							data-aos="fade-down"
 						>
 							<h3 className="text-3xl md:text-4xl font-black text-secondary-color">
 								Operations
